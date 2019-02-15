cd11de8bc91dc0bbc8f21cb65cb880866037e87f
ShakatarYT <shakataryt@gmail.com>
2019-02-15 15:56:32 +0100

    Added Example

---
+++ Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SD; //Import SD library
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StepDivider sd = new StepDivider(13, 3, false); //Create StepDivider object | 13/3 without steprest
+
+            foreach(Step s in sd) //Iterate over sd object which is Step[]
+            {
+                Console.WriteLine(Environment.NewLine + "-----------------------" + Environment.NewLine); //Display /r/n ----------------------- /r/n
+                Console.WriteLine(s.First + " TO " + s.Second); //Display {first number} TO {second number}
+                Console.WriteLine(Environment.NewLine + "-----------------------"); //Display /r/n -----------------------
+
+                //Next Step...
+            }
+        }
+    }
+}
